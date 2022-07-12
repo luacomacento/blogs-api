48936724d4ea0851486d98449eed298c31ac1a50
Luá <luacomacento@gmail.com>
2022-07-12 16:59:12 -0300

    feature: implementa models User, Category e BlogPost

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,34 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: {
+      type: DataTypes.STRING,
+      foreignKey: true,
+    },
+    image: DataTypes.STRING,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    timestamps: true,
+    updatedAt: 'updated',
+    createdAt: 'published',
+    tableName: 'BlogPosts',
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  }
+
+  return BlogPost;
+}
+
+module.exports = BlogPost;
---
+++ src/database/models/user.js
@@ -0,0 +1,27 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Users',
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',
+      as: 'blogPosts',
+    });
+  }
+
+  return User;
+}
+
+module.exports = User;
