927974a79947c22e0420cfd0f812c66847a7259e
luacomacento <luacomacento@gmail.com>
2022-08-11 19:19:09 -0300

    fix: fixes database name on sequelize config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -13,7 +13,7 @@ const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment] || suffix.test}`,
+    `${process.env.MYSQL_DB_NAME || 'blogs-api'}${suffix[environment]}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || '1234',
   dialect: 'mysql',
